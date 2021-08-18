cdabac14b3de17ef456bed647fa2a305be78b005
Phillip Luther <dev@phillipluther.com>
2021-08-17 22:13:29 -0700

    Add image processing for inline MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,11 @@ module.exports = {
         defaultLayouts: {
           default: require.resolve('./src/components/layout.js'),
         },
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+          },
+        ],
       },
     },
   ],
